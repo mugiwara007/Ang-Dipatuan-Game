f5e3a5183a01b64fcb3d6c99b42e37c38e248153
mugiwara007 <centeno.johnisrael.j.1911@gmail.com>
2021-10-13 22:10:22 +0800

    set fire on dipatuan sword when skill on

--- Ang Dipatuan/Assets/Scripts/MainCharacterScripts/Skills/DipatuanMode.cs
+++ Ang Dipatuan/Assets/Scripts/MainCharacterScripts/Skills/DipatuanMode.cs
@@ -11,23 +11,40 @@ public class DipatuanMode : MonoBehaviour
 
     DamageEnemy damageEnemyScript;
 
+    GameObject fireOnSword1, fireOnSword2;
+
+    private bool fireTurnOff = true;
+
     private void Awake()
     {
         damageEnemyScript = GameObject.FindGameObjectWithTag("KampilanArmed").GetComponent<DamageEnemy>();
         skillColorYellow = GameObject.FindGameObjectWithTag("DipatuanSkillYellowImage").GetComponent<Image>();
+
+        fireOnSword1 = GameObject.FindGameObjectWithTag("KampilanArmed").transform.GetChild(4).gameObject;
+
+        fireOnSword2 = GameObject.FindGameObjectWithTag("KampilanArmed").transform.GetChild(5).gameObject;
         onCooldown = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-       
-        
 
+
+        if (fireTurnOff)
+        {
+            fireOnSword1.SetActive(false);
+            fireOnSword2.SetActive(false);
+        }
 
         if (Input.GetButtonDown("DipatuanMode") && onCooldown == false)
         {
             damageEnemyScript.isDipatuanModeActivated = true;
+
+            fireOnSword1.SetActive(true);
+            fireOnSword2.SetActive(true);
+
+            fireTurnOff = false;
 
             damageEnemyScript.dmgPts = 30f;
 
@@ -52,9 +69,14 @@ public class DipatuanMode : MonoBehaviour
 
     IEnumerator DipatuanModeOff()
     {
-        yield return new WaitForSeconds(5f);
+        yield return new WaitForSeconds(7f);
 
         damageEnemyScript.isDipatuanModeActivated = false;
+
+        fireOnSword1.SetActive(false);
+        fireOnSword2.SetActive(false);
+
+        fireTurnOff = true;
     }
 
 }
